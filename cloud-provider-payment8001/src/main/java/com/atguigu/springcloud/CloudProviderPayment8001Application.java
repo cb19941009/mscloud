fc581956aa363e760324d689045e2896872d1a96
cb19941009 <1163646566@qq.com>
2022-07-14 00:15:18 +0800

    first

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/CloudProviderPayment8001Application.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/CloudProviderPayment8001Application.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author lixiaolong
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description 支付服务
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class CloudProviderPayment8001Application {
     public static void main(String[] args) {
         SpringApplication.run(CloudProviderPayment8001Application.class, args);
